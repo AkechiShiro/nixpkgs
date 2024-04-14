a2fb12bc20bc7c6a8d314eae2fdf432d0deb1052
V3ntus <29584664+V3ntus@users.noreply.github.com>
2024-04-13 22:10:45 -0500

    add wazuh-agent to module-list.nix

--- nixos/modules/module-list.nix
+++ nixos/modules/module-list.nix
@@ -1526,6 +1526,7 @@
   ./services/security/vaultwarden/default.nix
   ./services/security/warpgate.nix
   ./services/security/yubikey-agent.nix
+  ./services/security/wazuh-agent.nix
   ./services/system/automatic-timezoned.nix
   ./services/system/bpftune.nix
   ./services/system/cachix-agent/default.nix
