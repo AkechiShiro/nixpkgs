61b99943e1674528d185c681a1de24ca51c38f22
V3ntus <29584664+V3ntus@users.noreply.github.com>
2024-04-30 13:58:27 -0500

    Revert "chore: define top level wazuh package in by-name mechanism"
    This reverts commit fc2e300d42d57c7120e20c1bb1be54c6b4fa0d76.

--- pkgs/top-level/all-packages.nix
+++ pkgs/top-level/all-packages.nix
@@ -2362,6 +2362,8 @@ with pkgs;
 
   wayback = callPackage ../tools/misc/wayback { };
 
+  wazuh = callPackage ../tools/security/wazuh { };
+
   worker-build = callPackage ../development/tools/worker-build {
     inherit (darwin.apple_sdk.frameworks) Security;
   };
